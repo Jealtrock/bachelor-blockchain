4ec54dc1a6fc198ecd3d7a5e65f518ed8de1681c
Janusz <janusz-spatz@gmx.de>
2018-09-08 14:35:14 -0700

    iota api test; notizen dafür hinzugefügt

--- SmartRefueling/Raspberry Pi - Python/requesttest.py
+++ SmartRefueling/Raspberry Pi - Python/requesttest.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
 
-iota_seed = "DSPOAXMVSC99IUIVJXTIBZFATVFKTCLLJYOLAGSMFJGFXAWEB9GNTQWEDVRYHKIOQF9T9IZY9IVPKTSZ"
-print(iota_seed)
+#füge lokales Verzeichnis hinzu indem pyota installiert wurde
+import sys
+sys.path.append("/home/pi/.local/lib/python2.7/site-packages")
+
+#import iota
+from iota import Iota
+import logging
+
+seed = "DSPOAXMVSC99IUIVJXTIBZFATVFKTCLLJYOLAGSMFJGFXAWEB9GNTQWEDVRYHKIOQF9T9IZY9IVPKTSZ"
+#url = "http://iota.bitfinex.com:80"
+url = "https://field.deviota.com:443"
+
+api = Iota(url, seed)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+api.adapter.set_logger(logger)
+
+
