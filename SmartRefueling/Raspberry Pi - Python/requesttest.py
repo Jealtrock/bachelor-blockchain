5ba3679a5017f8a3ef9d12674075cd184aaa6f60
Janusz <janusz-spatz@gmx.de>
2018-09-14 11:46:38 -0700

    Smart Refueling app erstellt

--- SmartRefueling/Raspberry Pi - Python/requesttest.py
+++ SmartRefueling/Raspberry Pi - Python/requesttest.py
@@ -4,21 +4,22 @@
 import sys
 sys.path.append("/home/pi/.local/lib/python2.7/site-packages")
 
-#import iota
+#importe Iota-Bibliothek um die API anzusprechen
 from iota import Iota
+
 import logging
 
-seed = "DSPOAXMVSC99IUIVJXTIBZFATVFKTCLLJYOLAGSMFJGFXAWEB9GNTQWEDVRYHKIOQF9T9IZY9IVPKTSZ"
-#url = "http://iota.bitfinex.com:80"
+#lokal gespeicherter seed
+seed = "DSPOAXMVSC99IUIVJXTIBZFATVFKTCLLJYOLAGSMFJGFXAWEB9GNTQWEDVRYHKIOQF9T9IZY9IVPKTSZK"
+
 url = "https://field.deviota.com:443"
 
 api = Iota(url, seed)
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.setLevel(10)
 api.adapter.set_logger(logger)
 
-gna_result = api.get_new_addresses(count = 4)
+gna_result = api.get_new_addresses(count = 20)
 addresses = gna_result['addresses']
 print(addresses)
 
